077c1c76a864a975de4455a52226f9f04310f4fc
Philippe Vienne <philippe.vienne@bde-insa-lyon.fr>
2015-08-14 19:51:27 +0200

    New Menu Engine!

--- src/BdE/MainBundle/Menu/MenuBuilder.php
+++ src/BdE/MainBundle/Menu/MenuBuilder.php
@@ -2,27 +2,120 @@
 namespace BdE\MainBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 
 class MenuBuilder
 {
     private $factory;
+    /**
+     * @var int[]
+     */
+    private $dividerCounts;
+    /**
+     * @var \Symfony\Component\Security\Core\Authentication\Token\TokenInterface
+     */
+    private $token;
+    /**
+     * @var AuthorizationChecker
+     */
+    private $acl;
 
     /**
+     * @param TokenStorage $tokenStorage
+     * @param AuthorizationChecker $authorizationChecker
      * @param FactoryInterface $factory
      */
-    public function __construct(FactoryInterface $factory)
+    public function __construct(TokenStorage $tokenStorage, AuthorizationChecker $authorizationChecker,FactoryInterface $factory)
     {
+        $this->token = $tokenStorage->getToken();
+        $this->acl = $authorizationChecker;
         $this->factory = $factory;
     }
 
     public function createMainMenu(RequestStack $requestStack)
     {
+        $this->dividerCounts = array();
+
         $menu = $this->factory->createItem('root');
 
-        $menu->addChild('Home', array('route' => 'cva_gestion_membre_caca'));
-        // ... add more children
+        if(!$this->showMenu()){
+            return $menu;
+        }
+        if($this->acl->isGranted("ROLE_PERM"))
+            $menu->addChild('Ajouter Adhérant', array('route' => 'cva_gestion_membre_ajoutAdherent'));
+        if($this->acl->isGranted("ROLE_PERM")) {
+            $membres = $menu->addChild('Gestion des Membres');
+            $membres->addChild("CheckVA", array('route' => 'cva_gestion_membre_assoCheck'));
+            $membres->addChild("Actuels", array('route' => 'cva_gestion_membre_adherent'));
+            $membres->addChild("Anciens", array('route' => 'cva_gestion_membre_anciens'));
+        } else if($this->acl->isGranted("ROLE_CONSULT")) {
+            $menu->addChild("CheckVA", array('route' => 'cva_gestion_membre_assoCheck'));
+        }
+        if($this->acl->isGranted("ROLE_COWEI")) {
+            $wei = $menu->addChild('Zone CoWEI');
+            $wei->addChild("Pré-Inscrits", array('route' => 'bde_wei_inscription_préInscrits'));
+            $wei->addChild("Pré-Liste Attente", array('route' => 'bde_wei_inscription_listeAttentePre'));
+            $this->addDivider($wei);
+            $wei->addChild("Inscrits", array('route' => 'bde_wei_inscription_rechercheBizuthWEI'));
+            $wei->addChild("Liste Attente", array('route' => 'bde_wei_inscription_listeAttente'));
+            $this->addDivider($wei);
+            $wei->addChild("A rembourser",array('route'=>'bde_wei_inscription_remboursements'));
+        }
+        if($this->acl->isGranted("ROLE_ADMIN")) {
+            $admin = $menu->addChild('Zone Admin');
+            $admin->addChild("Créer Utilisateur", array('route' => ''));
+            $admin->addChild("Modifier Utilisateur", array('route' => ''));
+            $this->addDivider($admin);
+            $admin->addChild("Créer Produit", array('route' => ''));
+            $admin->addChild("Modifier Produit", array('route' => ''));
+        }
 
         return $menu;
+    }
+
+    public function createUserMenu(RequestStack $requestStack)
+    {
+        $this->dividerCounts = array();
+
+        $menu = $this->factory->createItem('root');
+
+        if(!$this->showMenu()){
+            return $menu;
+        }
+
+        $user = $menu->addChild($this->token->getUsername());
+
+        if($this->acl->isGranted("ROLE_EDIT_CONFIG")){
+            $user->addChild("Configuration",array("route"=>"cva_gestion_membre_config"));
+            if(!$this->acl->isGranted("ROLE_PERM"))
+                $this->addDivider($user);
+        }
+
+        if($this->acl->isGranted("ROLE_PERM")){
+            $user->addChild("Statistiques",array("route"=>"cva_gestion_membre_stats"));
+            $this->addDivider($user);
+        }
+
+        $user->addChild("Profil", array("route"=>"cva_gestion_membre_profil"));
+        $user->addChild("Deconnexion", array("route"=>"logout"));
+
+        return $menu;
+    }
+
+    private function showMenu(){
+        return $this->token->isAuthenticated() and $this->acl->isGranted("ROLE_USER");
+    }
+
+    private function addDivider(ItemInterface $menu){
+        $id = spl_object_hash($menu);
+        if (!array_key_exists($id,$this->dividerCounts)) {
+            $this->dividerCounts[$id] = 0;
+        }
+        $count = $this->dividerCounts[$id];
+        $menu->addChild("divider".strval($count))->setAttribute('divider',true);
+        $this->dividerCounts[$id]++;
     }
 }
