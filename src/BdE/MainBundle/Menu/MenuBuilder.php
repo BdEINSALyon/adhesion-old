da0cf582bbbc8aec0df7fa0f802f2f485c2cfc91
Philippe Vienne <philippe.vienne@bde-insa-lyon.fr>
2015-08-14 15:58:58 +0200

    Add the menu builder

---
+++ src/BdE/MainBundle/Menu/MenuBuilder.php
@@ -0,0 +1,28 @@
+<?php
+namespace BdE\MainBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', array('route' => 'cva_gestion_membre_caca'));
+        // ... add more children
+
+        return $menu;
+    }
+}
