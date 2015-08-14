479769722b8ceab12e840a7a72b4e92fbe32b1d4
Philippe Vienne <philippe.vienne@bde-insa-lyon.fr>
2015-08-14 21:14:36 +0200

    Made some improvements on the menu. Roles wasn't correctly defined

--- src/BdE/MainBundle/Menu/MenuBuilder.php
+++ src/BdE/MainBundle/Menu/MenuBuilder.php
@@ -49,9 +49,13 @@ class MenuBuilder
         if($this->acl->isGranted("ROLE_PERM")) {
             $membres = $menu->addChild('Gestion des Membres');
             $membres->addChild("CheckVA", array('route' => 'cva_gestion_membre_assoCheck'));
+            $this->addDivider($membres);
             $membres->addChild("Actuels", array('route' => 'cva_gestion_membre_adherent'));
             $membres->addChild("Anciens", array('route' => 'cva_gestion_membre_anciens'));
-        } else if($this->acl->isGranted("ROLE_CONSULT")) {
+        } else if($this->acl->isGranted("ROLE_SOIREE")) {
+            $menu->addChild("Verifier N°Etudiant", array('route' => 'cva_gestion_membre_assoCheck'));
+            $menu->addChild("Liste Membres Actuels", array('route' => 'cva_gestion_membre_adherent'));
+        } else if($this->acl->isGranted("ROLE_CONSULT")){
             $menu->addChild("CheckVA", array('route' => 'cva_gestion_membre_assoCheck'));
         }
         if($this->acl->isGranted("ROLE_COWEI")) {
@@ -63,6 +67,10 @@ class MenuBuilder
             $wei->addChild("Liste Attente", array('route' => 'bde_wei_inscription_listeAttente'));
             $this->addDivider($wei);
             $wei->addChild("A rembourser",array('route'=>'bde_wei_inscription_remboursements'));
+            if($this->acl->isGranted("ROLE_EDIT_CONFIG") && !$this->acl->isGranted("ROLE_ADMIN")) {
+                $this->addDivider($wei);
+                $wei->addChild("Configuration WEI",array("route"=>"cva_gestion_membre_config"));
+            }
         }
         if($this->acl->isGranted("ROLE_ADMIN")) {
             $admin = $menu->addChild('Zone Admin');
@@ -71,6 +79,8 @@ class MenuBuilder
             $this->addDivider($admin);
             $admin->addChild("Créer Produit", array('route' => 'cva_gestion_membre_addProduit'));
             $admin->addChild("Modifier Produit", array('route' => 'cva_gestion_membre_tableauProduits'));
+            $this->addDivider($admin);
+            $admin->addChild("Configuration Général",array("route"=>"cva_gestion_membre_config"));
         }
 
         return $menu;
@@ -86,18 +96,12 @@ class MenuBuilder
             return $menu;
         }
 
-        $user = $menu->addChild($this->token->getUsername());
-
-        if($this->acl->isGranted("ROLE_EDIT_CONFIG")){
-            $user->addChild("Configuration",array("route"=>"cva_gestion_membre_config"));
-            if(!$this->acl->isGranted("ROLE_PERM"))
-                $this->addDivider($user);
-        }
-
         if($this->acl->isGranted("ROLE_PERM")){
-            $user->addChild("Statistiques",array("route"=>"cva_gestion_membre_stats"));
-            $this->addDivider($user);
+            $menu->addChild("Statistiques",array("route"=>"cva_gestion_membre_stats"));
+            $this->addDivider($menu);
         }
+
+        $user = $menu->addChild($this->token->getUsername());
 
         $user->addChild("Profil", array("route"=>"cva_gestion_membre_profil"));
         $user->addChild("Deconnexion", array("route"=>"logout"));
