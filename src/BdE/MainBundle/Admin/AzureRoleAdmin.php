1960d68af3276d1a13c565637f94aa129391d397
Philippe Vienne <philippe@vienne.me>
2015-08-21 09:29:11 +0200

    Solve Type bug on a function call

--- src/BdE/MainBundle/Admin/AzureRoleAdmin.php
+++ src/BdE/MainBundle/Admin/AzureRoleAdmin.php
@@ -8,7 +8,7 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\CoreBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Validator\ErrorElement;
 
 class AzureRoleAdmin extends Admin
 {
